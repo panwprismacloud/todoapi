f8ce706d9f02dfce2c0a7ebedb1518011798ed04
Álvaro <alvarofg@gmail.com>
2024-11-12 17:32:29 +0100

    Create vuln.tf
    adding open s3

---
+++ vuln.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "public_bucket" {
+  bucket = "mi-bucket-publico"
+  acl    = "public-read"  # Establece permisos de lectura pública en el bucket
+
+  website {
+    index_document = "index.html"
+  }
+}
+
+# Política para permitir acceso público a los objetos dentro del bucket
+resource "aws_s3_bucket_policy" "public_access_policy" {
+  bucket = aws_s3_bucket.public_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.public_bucket.arn}/*"
+      }
+    ]
+  })
+}
